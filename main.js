8714b66b8f860be8c4fa0a4a27d4fb832e194d92
Tiago <84323433+custodiotiago1986@users.noreply.github.com>
2024-08-04 21:56:46 -0300

    UPDATE URL do Backend

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const postForm = document.getElementById('postForm');
     const welcomeMessage = document.getElementById('welcomeMessage');
     const welcomeUsername = document.getElementById('welcomeUsername');
-    const baseUrl = 'https://sitepriscilabackend-bcengybre2gmashv.eastus-01.azurewebsites.net'; // URL do backend
+    const baseUrl = 'sitepriscilabackend-bcengybre2gmashv.brazilsouth-01.azurewebsites.net'; // URL do backend
 
     function checkLoginStatus() {
         const username = localStorage.getItem('username');
